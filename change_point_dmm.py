44670cee7bb6b037ad90a7f4465a4b91be8d7b82
Hiroki Tanaka <rokitaniplus1@gmail.com>
2022-10-05 11:31:18 +0900

    deleted long span models

--- change_point_dmm.py
+++ change_point_dmm.py
@@ -65,32 +65,6 @@ class change_point_DMM(nn.Module):
             )
             z_prev = z_t
 
-    def model_long_span(self,data,term=5):        
-        pyro.module("dmm", self)
-        pi = torch.tensor([1.0]*term)
-        cp = pyro.sample("C",dist.Categorical(torch.softmax(pi,0,dtype=torch.double)))
-        z_prev = self.z_0
-        for t in range(1, 200*cp+1):
-            z_loc = self.transition_before(z_prev)
-            z_t = pyro.sample("Z_%d" % t,dist.Normal(z_loc,0.01).to_event(1))           
-            emission_probs_t = self.emittion(z_t)
-            pyro.sample(
-                "X_%d" % t,
-                dist.Normal(emission_probs_t,0.01).to_event(1),
-                obs=data[t]
-            )
-            z_prev = z_t
-        for t in range(200*cp+1, self.length):
-            z_loc = self.transition_after(z_prev)
-            z_t = pyro.sample("Z_%d" % t,dist.Normal(z_loc,0.01).to_event(1))           
-            emission_probs_t = self.emittion(z_t)
-            pyro.sample(
-                "X_%d" % t,
-                dist.Normal(emission_probs_t,0.01).to_event(1),
-                obs=data[t]
-            )
-            z_prev = z_t
-
     def guide(self,data):
         pyro.module("dmm", self)
         pi = pyro.param('pi',lambda: torch.tensor([1.0]*self.length))
@@ -100,16 +74,7 @@ class change_point_DMM(nn.Module):
         for t in range(1, self.length):
             z_loc = rnn_output[t-1]
             pyro.sample("Z_%d" % t,dist.Normal(z_loc,0.01).to_event(1))
-    def long_span_guide(self,data,term=5):
-        pyro.module("dmm", self)
-        pi = pyro.param('pi',lambda: torch.tensor([1.0]*term))
-        pyro.sample("C", dist.Categorical(torch.softmax(pi,0,torch.double)))
-        h0 = self.h_0
-        rnn_output, _ = self.rnn(data,h0)      
-        for t in range(1, self.length):
-            z_loc = rnn_output[t-1]
-            pyro.sample("Z_%d" % t,dist.Normal(z_loc,0.01).to_event(1))
-
+    
       
 if __name__ == "__main__":
     change_point_DMM(100)
